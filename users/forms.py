de50d582da3c6303e32e8893ed766dae5471578c
notarious <notarious2@gmail.com>
2022-12-12 09:42:15 +0500

    finished registration page

--- users/forms.py
+++ users/forms.py
@@ -5,10 +5,17 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-    username = forms.CharField(required=True)
-    password1 = forms.PasswordInput()
-    password2 = forms.PasswordInput()
+    email = forms.EmailField(required=True, widget=forms.EmailInput(
+        attrs={'class': 'w-64 px-3 py-2 rounded-md border border-slate-400', 'placeholder': 'Your Email'}))
+    username = forms.CharField(required=True, widget=forms.TextInput(
+        attrs={'class': 'w-64 px-3 py-2 rounded-md border border-slate-400', 'placeholder': 'Your Username',
+               }))
+    password1 = forms.CharField(required=True, widget=forms.PasswordInput(
+        attrs={'class': 'w-64 px-3 py-2 rounded-md border border-slate-400', 'placeholder': 'Your Password',
+               }))
+    password2 = forms.CharField(required=True, widget=forms.PasswordInput(
+        attrs={'class': 'w-64 px-3 py-2 rounded-md border border-slate-400', 'placeholder': 'Re-type Password',
+               }))
 
     class Meta:
         model = User
@@ -29,9 +36,9 @@ class CustomLoginForm(AuthenticationForm):
         super().__init__(*args, **kwargs)
         self.fields['username'].widget.attrs.update(
             {'placeholder': 'Your Username',
-             'class': 'w-96 px-3 py-2 rounded-md border border-slate-400'}
+             'class': 'w-64 px-3 py-2 rounded-md border border-slate-400'}
         )
         self.fields['password'].widget.attrs.update(
             {'placeholder': 'Your Password',
-             'class': 'w-96 px-3 py-2 rounded-md border border-slate-400'}
+             'class': 'w-64 px-3 py-2 rounded-md border border-slate-400'}
         )
