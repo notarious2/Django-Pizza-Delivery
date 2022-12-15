207ac13df7c892e81b7a8b4139dcd0a12b48794d
notarious <notarious2@gmail.com>
2022-12-15 23:20:59 +0500

    added product variants model

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Size, ProductVariant
 # Register your models here.
 
 admin.site.register(Product)
+admin.site.register(Size)
+admin.site.register(ProductVariant)
--- store/models.py
+++ store/models.py
@@ -9,9 +9,9 @@ class Product(models.Model):
         ("Pizza", "pizza"),
         ("Nonpizza", "nonpizza")
     )
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
-    price = models.IntegerField()
     desc = models.TextField(max_length=500)
     image = models.ImageField(blank=True, upload_to='images')
     created_at = models.DateField(auto_now_add=True)
@@ -20,3 +20,46 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    @property
+    def get_product_variants(self):
+        # reverse accessor
+        variants = self.productvariant_set.all()
+        variants_dict = {}
+        counter = 0
+        for variant in variants:
+            variants_dict[counter] = str(variant.size)
+            counter += 1
+        print(variants_dict)
+        print("WTF")
+        return variants
+
+    @property
+    def has_variants(self):
+        if self.productvariant_set.count() > 0:
+            return True
+        else:
+            return False
+
+    @property
+    def check_variant(self):
+        # reverse accessor
+        print(self.productvariant_set.size())
+        return True
+
+
+class Size(models.Model):
+    name = models.CharField(max_length=20, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductVariant(models.Model):
+    title = models.CharField(max_length=100, blank=True, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    size = models.ForeignKey(Size, on_delete=models.CASCADE)
+    price = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.title} - price: ${self.price}"
