d738c85d14f9e86eec1ae3ac4b1c401a43628805
notarious <notarious2@gmail.com>
2023-01-10 15:07:46 +0500

    changed context processor, and added prettier ignore in products page

--- store/context_processors.py
+++ store/context_processors.py
@@ -16,16 +16,12 @@ def get_cart_quantity(request):
             customer, created = Customer.objects.get_or_create(
                 device=request.COOKIES['device'])
         except:
-            return {
-                'cart_quantity': 0
-            }
+            number_of_items = 0
     try:
         customer_order = Order.objects.filter(
             customer=customer, complete=False)[0]
         number_of_items = customer_order.get_cart_items
     except:
         number_of_items = 0
-    finally:
-        return {
-            'cart_quantity': number_of_items
-        }
+
+    return {'cart_quantity': number_of_items}
