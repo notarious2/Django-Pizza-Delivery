fcd43db2936b28d62fd4da226fd5944db2da01be
notarious <notarious2@gmail.com>
2023-01-11 17:18:54 +0500

    moved secret keys to environmental variable

--- epizza/settings.py
+++ epizza/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,14 +26,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-()#2&yc2lbs@=fsizbjoca+m=r3n8)!$f(#&(s%x1242r3ckah'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 if DEBUG:
-    STRIPE_PUBLISHABLE_KEY = 'pk_test_51MARD5DKgg2oTDzQikYctOULHSmwrWKGArDqgXhBzBc0c1T1mD9ycLr71TDhZEAQOyp98Mm7Zkz5fqUN8wr5BQ7o001ViVAGKQ'
-    STRIPE_SECRET_KEY = 'sk_test_51MARD5DKgg2oTDzQTZaScLCenIw2LRBIiuA4Gz8tsI6In2sATtiilIh9j0zd212Pr3Y9be8Xdp925PVf7KNfj27q00ScR5qBP9'
+    STRIPE_PUBLISHABLE_KEY = os.environ.get("STRIPE_PUBLISHABLE_KEY")
+    STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
 
 ALLOWED_HOSTS = []
 
