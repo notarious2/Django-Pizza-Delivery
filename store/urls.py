856cec1e566a3e9e82c7d444e05ebcca5ad691ea
notarious1~git config --global user.email notarious2@gmail.comgit config --global user.name notarious <notarious2@gmail.com>
2023-01-17 13:22:08 +0500

    Removed Dashboard, including create, update and delete product views

--- store/urls.py
+++ store/urls.py
@@ -7,10 +7,4 @@ urlpatterns = [
     path('pizza/', views.pizzas, name='pizzas'),
     path('drinks/', views.drinks, name='drinks'),
     path('sides/', views.sides, name='sides'),
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('products/add/', views.ProductCreateView.as_view(), name='add_product'),
-    path('products/update/<uuid:pk>/',
-         views.ProductUpdateView.as_view(), name='update_product'),
-    path('products/delete/<uuid:pk>/',
-         views.ProductDeleteView.as_view(), name='delete_product'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,23 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from .models import Product
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
-from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
-from django.contrib.auth.decorators import user_passes_test
 from django.core.paginator import Paginator
-
-
-def index(request):
-    return render(request, 'store/index.html')
-
-
-@user_passes_test(lambda user: user.is_superuser, login_url="store:products")
-def dashboard(request):
-    """ using function-based view for dashboard as it promptly
-    updates context data """
-    products = Product.objects.all()
-    context = {'products': products}
-    return render(request, 'store/dashboard.html', context)
 
 
 def products(request):
@@ -57,39 +40,3 @@ def sides(request):
     products = Product.objects.filter(product_category__name="Side")
     context = {'products': products}
     return render(request, 'store/sides.html', context=context)
-
-
-class SuperUserRequiredMixin(LoginRequiredMixin, UserPassesTestMixin):
-    """A custom mixin that checks if user is both logged in and superuser, then allows accessing the template
-    if user is not a supersuer, then it redirects to the products page
-    """
-
-    def test_func(self):
-        return self.request.user.is_superuser
-
-    def handle_no_permission(self):
-        return redirect('store:products')
-
-
-class ProductCreateView(SuperUserRequiredMixin, CreateView):
-    model = Product
-    fields = ['name', 'price', 'desc', 'image']
-
-    # redirects to the dashboard
-    def get_success_url(self):
-        return reverse_lazy('store:dashboard')
-
-
-class ProductUpdateView(SuperUserRequiredMixin, UpdateView):
-    model = Product
-    fields = ['name', 'price', 'desc', 'image', 'product_type']
-    template_name_suffix = '_update_form'
-
-    # redirects to the dashboard
-    def get_success_url(self):
-        return reverse_lazy('store:dashboard')
-
-
-class ProductDeleteView(DeleteView):
-    model = Product
-    success_url = reverse_lazy('store:dashboard')
