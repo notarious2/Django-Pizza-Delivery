7f1a65589433fd7b49aa0b39988482d326ed2258
notarious1~git config --global user.email notarious2@gmail.comgit config --global user.name notarious <notarious2@gmail.com>
2023-01-19 17:45:55 +0500

    added url tests for users app

---
+++ users/tests/test_urls.py
@@ -0,0 +1,27 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from users.views import SignUpView, MyLoginView, logout_view, my_orders
+
+
+class TestUserUrls(SimpleTestCase):
+    """Test that User URLs are resolved"""
+
+    def test_register_url_is_resolved(self):
+        """Test register url is resolved"""
+        url = reverse('users:register')
+        self.assertEqual(resolve(url).func.view_class, SignUpView)
+
+    def test_login_url_is_resolved(self):
+        """Test login url is resolved"""
+        url = reverse('users:login')
+        self.assertEqual(resolve(url).func.view_class, MyLoginView)
+
+    def test_logout_url_is_resolved(self):
+        """Test logout url is resolved"""
+        url = reverse('users:logout')
+        self.assertEqual(resolve(url).func, logout_view)
+
+    def test_my_orders_url_is_resolved(self):
+        """Test My Orders url is resolved"""
+        url = reverse('users:my_orders')
+        self.assertEqual(resolve(url).func, my_orders)
